be93d33144eb4c790bb9b4523b91cd2a30110c9c
kendrick807 <tkogt12@gmail.com>
2023-09-23 16:15:04 +0300

    FPV-11 Added Counter.js to auto-increment id

--- web-intf/backend/models/user.js
+++ web-intf/backend/models/user.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt")
 const Schema = mongoose.Schema;
+const Counter = require("./Counter");
 
-//defining user schema
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  id: { type: Number, required: true },
-  fingerPrint: { type: String, required: false},
-  candidateVotedFor: [{type: Schema.ObjectId, ref: 'candidate', required: false}],
+  id: { type: Number, required: true, default: -1 },
+  fingerPrint: { type: String, required: false, default: ""},
+  candidateVotedId: {type: Number, required: false, default: null},
   isAdmin: { type: Boolean, required: true},
+});
+
+userSchema.pre("save", function(next) {
+  Counter.findByIdAndUpdate({_id: 'userId'}, {$inc: { seq: 1} }).then((idCounter) => {
+      this.id = idCounter.seq;
+      next();
+  }).catch((error) => {
+    if(error){
+      return next(error);
+    }
+  })
 });
 
 userSchema.pre("save", async function(next) {
@@ -23,5 +34,4 @@ userSchema.pre("save", async function(next) {
     }
   });
 
-//Export model
 module.exports = mongoose.model("User", userSchema);
