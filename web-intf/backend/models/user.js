20507db09b4bf1f6571849f5db60b5601352ac2a
kendrick807 <tkogt12@gmail.com>
2023-09-24 12:45:23 +0300

    Minor fixes, now hashes password in route.js

--- web-intf/backend/models/user.js
+++ web-intf/backend/models/user.js
@@ -23,15 +23,5 @@ userSchema.pre("save", function(next) {
   })
 });
 
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    try {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-      return next();
-    } catch (error) {
-      return next(error);
-    }
-  });
 
 module.exports = mongoose.model("User", userSchema);
--- web-intf/backend/routes/routes.js
+++ web-intf/backend/routes/routes.js
@@ -5,25 +5,27 @@ const bcrypt = require("bcrypt");
 
 router.post('/register', async (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    let password = req.body.password;
 
     try {
         const user = await User.find({ username: username });
         if (user.length === 0) {
+            const salt = await bcrypt.genSalt(10);
+            password = await bcrypt.hash(password, salt);
             const newUser = new User({ username: username, password: password, isAdmin: false });
             let userSaved = await newUser.save();
             if (userSaved != {}) {
                 res.status(200).send({ message: "User saved successfully!" });
             }
             else {
-                return res.status(500).send({ error: "Error: Error saving user!" });
+                return res.status(500).send({ error: "Unable To Create User!" });
             }
         } else {
             return res.status(400).send({ error: "Error: User already exists!" });
         }
     } catch (error) {
         console.log(error);
-        return res.status(500).send({ error: "Server Internal Error" });
+        return res.status(500).send({ error: "Unable To Create User!" });
     }
 })
 
@@ -36,18 +38,18 @@ router.post('/login', async (req, res) => {
         if (user.length === 1) {
             const match = await bcrypt.compare(password,user[0].password);
             if(match){
-                res.status(200).send()
+                res.status(200).send({ message: "Login successful!" });
             }else{
                 return res.status(400).send({error: "Error: Incorrect Password!"})
             }
         }
         else if (user.length > 1) {
-            return res.status(500).send({ error: "Error: Error logging in!" });
+            return res.status(500).send({ error: "Error: Multiple Username exists!" });
         } else if (user.length === 0) {
             return res.status(400).send({ error: "Error: User Not Found!" });
         }
     } catch (error) {
-        return res.status(500).send({ error: "Server Internal Error" });
+        return res.status(500).send({ error: "Error: Unable To Login!" });
     }
 });
 
