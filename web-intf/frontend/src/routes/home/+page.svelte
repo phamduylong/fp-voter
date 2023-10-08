05e04d3538beb0dce4f5032216f28267e6f6ebb1
longph <longphamduy2002@gmail.com>
2023-10-08 20:30:53 +0300

    FPV-19 home page nav and code clean up

--- web-intf/frontend/src/routes/home/+page.svelte
+++ web-intf/frontend/src/routes/home/+page.svelte
@@ -1,82 +1,81 @@
 <script>
-    import { onMount } from "svelte";
-    import { goto } from "$app/navigation";
-    import { AppShell } from "@skeletonlabs/skeleton";
-    import { AppBar } from "@skeletonlabs/skeleton";
+  import { goto } from "$app/navigation";
+  import {
+    AppShell,
+    ListBox,
+    ListBoxItem,
+    popup,
+  } from "@skeletonlabs/skeleton";
 
-    let token =
-        typeof window !== "undefined" ? localStorage.getItem("jwt") : null;
+  $: token = typeof window !== "undefined" ? localStorage.getItem("jwt") : null;
 
-    function isTokenExpired(token) {
-        if (!token) return true;
-        const decodedToken = JSON.parse(atob(token.split(".")[1])); // Decode the token
-        const expirationTime = decodedToken.exp * 1000;
-        const currentTime = Date.now();
-        return currentTime > expirationTime;
-    }
-    function handleExpiredToken() {
-        if (isTokenExpired(token)) {
-            localStorage.removeItem("jwt");
-            token = null;
-            window.location.href = "http://localhost:8081/login"
-        }
-    }
+  let comboboxValue = "";
 
-    async function handleLogout() {
-        await fetch("http://localhost:8080/logout", {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-        })
-            .then(async (res) => {
-                if (res.status === 200) {
-                    localStorage.removeItem("jwt");
-                    token = null;
-                }
-                await goto("/login");
-            })
-            .catch((err) => {
-                // also a modal to tell user the error
-                console.error(err);
-            });
-    }
+  const popupCombobox = {
+    event: "click",
+    target: "popupCombobox",
+    placement: "bottom",
+    closeQuery: "",
+  };
 
-    onMount(async () => {
-        handleExpiredToken();
-        if (!token) {
-            await goto("/login");
-        } else {
-            const decodedToken = JSON.parse(atob(token.split(".")[1]));
-            console.log(decodedToken.user);
-        }
-    });
-
-
+  async function handleLogout() {
+    await fetch("http://localhost:8080/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(async (res) => {
+        if (res.status === 200) localStorage.removeItem("jwt");
+        await goto("/login");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
-    <svelte:fragment slot="header">
-        <AppBar
-            ><strong class="text-xl uppercase">FP Voter</strong>
-
-                </AppBar
-        >
-    </svelte:fragment>
-
-    <svelte:fragment slot="sidebarLeft">
-        <nav class="list-nav">
-            <ul>
-                <li><a href="/home" on:click={handleExpiredToken}>Home</a></li>
-                <li><a href="/vote" on:click={handleExpiredToken}>Vote</a></li>
-                <li><a href="/" on:click={handleLogout}>Logout</a></li>
-            </ul>
-        </nav>
-    </svelte:fragment>
-
+<AppShell>
+  <div slot="header">
+    <nav class="list-nav flex justify-center w-full py-4 card">
+      <a href="/home">Home</a>
+      <a href="/vote">Vote</a>
+      <a href="/results">Results</a>
+      <a href="/candidates">Candidates</a>
+      <button
+        class="btn variant-filled absolute right-10 !bg-inherit hover:!bg-gray-800 !text-inherit border-2"
+        use:popup={popupCombobox}
+      >
+        Hello, username
+        <div class="card py-2" data-popup="popupCombobox">
+          <ListBox>
+            <ListBoxItem
+              bind:group={comboboxValue}
+              name="medium"
+              value="profile"
+              ><a href="/profile" class="anchor hover:!bg-transparent"
+                >Profile</a
+              ></ListBoxItem
+            >
+            <ListBoxItem
+              bind:group={comboboxValue}
+              name="medium"
+              value="logout"
+              on:click={handleLogout}
+            >
+              <a
+                class="anchor hover:!bg-transparent"
+                href="/"
+                on:click|preventDefault={handleLogout}>Logout</a
+              >
+            </ListBoxItem>
+          </ListBox>
+        </div>
+      </button>
+    </nav>
+  </div>
 </AppShell>
 
 <style>
-
 </style>
