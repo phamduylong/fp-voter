4c1db5c6ed2304068e10bbb3e2a5acf22eae945a
Long Pham <69810988+phamduylong@users.noreply.github.com>
2023-10-19 22:24:09 +0300

    FPV-14 add build script and github workflow script (#19)
    * FPV-14 add build script and github workflow script
    
    * FPV-14 add script
    
    * FPV-14 update script add cache-dependency-path
    
    * FPV-14 Rename user.js to User.js
    
    * FPV-14 remove node 14 as bcrypt seems to not be supported
    
    * FPV-14 add pipeline run on feature branch pushes
    
    * FPV-14 add environment variables from repo secrets
    
    * FPV-14 add rm PORT env var
    
    * FPV-14 add double quotes in attempt to fix formatting issues
    
    * FPV-14 test mongo uri as literal string
    
    * FPV-14 test mongo uri as literal string
    
    * FPV-14 add env var
    
    * FPV-14 set env vars in a separate job
    
    * FPV-14 set env var inside step instructions
    
    * FPV-14 kill process after build succeeded
    
    * FPV-14 add env var for testing job
    
    * FPV-14 kill process after testing step
    
    * FPV-14 kill process after testing step 2nd version

--- web-intf/backend/utilities/utilities.js
+++ web-intf/backend/utilities/utilities.js
@@ -2,33 +2,28 @@ const JWT = require('../models/JWT');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
-
 dotenv.config();
 const JWT_KEY = process.env.JWT_SECRET;
 
 const checkJwtExpiration = async (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
-    if (token) {
+    if (token !== "null" && token !== undefined) {
         try {
             const decodedToken = jwt.verify(token, JWT_KEY);
-            const matchToken = await checkInactiveToken(token);
-
-            if (matchToken === true) {
-                return res.status(401).json({ message: 'Token is inactive' });
+            const tokenIsInactive = await checkInactiveToken(token);
+            const currentTokenExpired = decodedToken.exp * 1000 < Date.now();
+            if (tokenIsInactive || currentTokenExpired) {
+                return res.status(401).json({ error: 'Session has expired. Please log in again.' });
             }
-            else if (decodedToken.exp * 1000 < Date.now()) {
-                // Token has expired, send a 401 Unauthorized response
-                return res.status(401).json({message: 'Token has expired'});
-            }
-
-            // Token is still valid, continue with the request
             req.user = decodedToken;
             next();
         } catch (err) {
-            return res.status(401).json({ message: 'Invalid token' });
+            // in theory, this should not happen for ordinary users
+            return res.status(401).json({ error: 'JWT malformed' });
         }
     } else {
-        return res.status(401).json({ message: 'Token not provided' });
+        //token was not provided, user not logged in.
+        return res.status(401).json({ error: 'You are not logged in.' });
     }
 };
 async function deleteExpiredTokens() {
@@ -57,7 +52,6 @@ function checkUserValidations(username, password){
     const passwordRegex = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!*_])([A-Za-z\d@#$%^&+=!*_]){8,20}$/);
     const isPwdMatch = passwordRegex.test(password);
     return (isUsernameMatch && isPwdMatch)
-
 }
 
 module.exports = {checkJwtExpiration, checkUserValidations}
